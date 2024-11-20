3f274342e3803c6e0574d22e7b342411152f232c
Megiyanto377 <102566011+Megiyanto377@users.noreply.github.com>
2024-11-20 09:40:05 +0800

    Update video_cnn.py

--- model/video_cnn.py
+++ model/video_cnn.py
@@ -16,54 +16,6 @@ def conv3x3(in_planes, out_planes, stride=1):
 def conv1x1(in_planes, out_planes, stride=1):
     return nn.Conv2d(in_planes, out_planes, kernel_size=1)
 
-
-# class ChannelAttention(nn.Module):
-#     def __init__(self, num_channels, reduction_ratio=16):
-#         super(ChannelAttention, self).__init__()
-#         self.avg_pool = nn.AdaptiveAvgPool2d(1)
-#         self.max_pool = nn.AdaptiveMaxPool2d(1)
-#         self.fc = nn.Sequential(
-#             nn.Linear(num_channels, num_channels // reduction_ratio, bias=False),
-#             nn.ReLU(inplace=True),
-#             nn.Linear(num_channels // reduction_ratio, num_channels, bias=False)
-#         )
-#         self.sigmoid = nn.Sigmoid()
-
-#     def forward(self, x):
-#         b, c, _, _ = x.shape
-#         # Ensure that x is reshaped correctly for the Linear layers
-#         avg_out = self.fc(self.avg_pool(x).view(b, c))
-#         max_out = self.fc(self.max_pool(x).view(b, c))
-#         out = avg_out + max_out
-#         return self.sigmoid(out).view(b, c, 1, 1)
-
-
-
-# class SpatialAttention(nn.Module):
-#     def __init__(self, kernel_size=7):
-#         super(SpatialAttention, self).__init__()
-#         assert kernel_size % 2 == 1, "Kernel size must be odd"
-#         self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=kernel_size//2, bias=False)
-#         self.sigmoid = nn.Sigmoid()
-
-#     def forward(self, x):
-#         avg_out = torch.mean(x, dim=1, keepdim=True)
-#         max_out, _ = torch.max(x, dim=1, keepdim=True)
-#         x = torch.cat([avg_out, max_out], dim=1)
-#         x = self.conv1(x)
-#         return self.sigmoid(x)
-
-# class CBAM(nn.Module):
-#     def __init__(self, num_channels, reduction_ratio=16, kernel_size=7):
-#         super(CBAM, self).__init__()
-#         self.channel_attention = ChannelAttention(num_channels, reduction_ratio)
-#         self.spatial_attention = SpatialAttention(kernel_size)
-
-#     def forward(self, x):
-#         x = x * self.channel_attention(x)
-#         x = x * self.spatial_attention(x)
-#         return x
-        
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -71,18 +23,14 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = conv3x3(inplanes, planes, stride)
         self.bn1 = nn.BatchNorm2d(planes)
-        self.relu = nn.ReLU(inplace=True)
+        # self.relu = nn.ReLU(inplace=True)
+        self.relu = nn.PReLU()
         self.conv2 = conv3x3(planes, planes)
         self.bn2 = nn.BatchNorm2d(planes)
         self.downsample = downsample
         self.stride = stride
         self.se = se
         
-        # if(self.se):
-        #     self.gap = nn.AdaptiveAvgPool2d(1)
-        #     self.conv3 = conv1x1(planes, planes//16)
-        #     self.conv4 = conv1x1(planes//16, planes)
-
         if(self.se):
             # self.se = SEBlock(planes)
             self.enhanced_se = EnhancedSEBlock(planes)
@@ -99,16 +47,6 @@ class BasicBlock(nn.Module):
         if self.downsample is not None:
             residual = self.downsample(x)
             
-        # if(self.se):
-        #     w = self.gap(out)
-        #     w = self.conv3(w)
-        #     w = self.relu(w)
-        #     w = self.conv4(w).sigmoid()
-            
-        #     out = out * w
-
-        # seblock
-        # print(out.shape)
         if self.se:
             # out = self.se(out)
             out = self.enhanced_se(out)
@@ -119,95 +57,6 @@ class BasicBlock(nn.Module):
 
         return out
 
-# # original seblock
-# class SEBlock(nn.Module):
-#     def __init__(self, planes, reduction=16):
-#         super(SEBlock, self).__init__()
-#         self.gap = nn.AdaptiveAvgPool2d(1)
-#         self.conv1 = conv1x1(planes, planes // reduction)
-#         self.conv2 = conv1x1(planes // reduction, planes)
-#         self.relu = nn.ReLU(inplace=True)
-        
-#     def forward(self, x):
-#         w = self.gap(x)
-#         w = self.conv1(w)
-#         w = self.relu(w)
-#         w = self.conv2(w).sigmoid()
-#         return x * w
-
-# # enhanced seblock
-# class ChannelGate(nn.Module):
-#     def __init__(self, gate_channels, reduction_ratio=16, pool_types=['avg', 'max']):
-#         super(ChannelGate, self).__init__()
-#         self.gate_channels = gate_channels
-#         self.mlp = nn.Sequential(
-#             Flatten(),
-#             nn.Linear(gate_channels, gate_channels // reduction_ratio),
-#             nn.ReLU(),
-#             nn.Linear(gate_channels // reduction_ratio, gate_channels)
-#             )
-#         self.pool_types = pool_types
-#     def forward(self, x):
-#         channel_att_sum = None
-#         for pool_type in self.pool_types:
-#             if pool_type=='avg':
-#                 avg_pool = F.avg_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-#                 channel_att_raw = self.mlp( avg_pool )
-#             elif pool_type=='max':
-#                 max_pool = F.max_pool2d( x, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-#                 channel_att_raw = self.mlp( max_pool )
-#             elif pool_type=='lp':
-#                 lp_pool = F.lp_pool2d( x, 2, (x.size(2), x.size(3)), stride=(x.size(2), x.size(3)))
-#                 channel_att_raw = self.mlp( lp_pool )
-#             elif pool_type=='lse':
-#                 # LSE pool only
-#                 lse_pool = logsumexp_2d(x)
-#                 channel_att_raw = self.mlp( lse_pool )
-
-#             if channel_att_sum is None:
-#                 channel_att_sum = channel_att_raw
-#             else:
-#                 channel_att_sum = channel_att_sum + channel_att_raw
-
-#         scale = F.sigmoid( channel_att_sum ).unsqueeze(2).unsqueeze(3).expand_as(x)
-#         return x * scale
-
-
-# class SpatialGate(nn.Module):
-#     def __init__(self):
-#         super(SpatialGate, self).__init__()
-#         kernel_size = 7
-#         self.compress = ChannelPool()
-#         self.spatial = BasicConv(2, 1, kernel_size, stride=1, padding=(kernel_size-1) // 2, relu=False)
-#     def forward(self, x):
-#         x_compress = self.compress(x)
-#         x_out = self.spatial(x_compress)
-#         scale = F.sigmoid(x_out) # broadcasting
-#         return x * scale
-
-# class Flatten(nn.Module):
-#     def forward(self, x):
-#         return x.view(x.size(0), -1)
-# class ChannelPool(nn.Module):
-#     def forward(self, x):
-#         return torch.cat( (torch.max(x,1)[0].unsqueeze(1), torch.mean(x,1).unsqueeze(1)), dim=1 )
-# class BasicConv(nn.Module):
-#     def __init__(self, in_planes, out_planes, kernel_size, stride=1, padding=0, dilation=1, groups=1, relu=True, bn=True, bias=False):
-#         super(BasicConv, self).__init__()
-#         self.out_channels = out_planes
-#         self.conv = nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
-#         self.bn = nn.BatchNorm2d(out_planes,eps=1e-5, momentum=0.01, affine=True) if bn else None
-#         self.relu = nn.ReLU() if relu else None
-
-#     def forward(self, x):
-#         x = self.conv(x)
-#         if self.bn is not None:
-#             x = self.bn(x)
-#         if self.relu is not None:
-#             x = self.relu(x)
-#         return x
-
-
 
 class EnhancedSEBlock(nn.Module):
     def __init__(self, planes, reduction=16):
@@ -215,7 +64,7 @@ class EnhancedSEBlock(nn.Module):
         self.channel_se = nn.Sequential(
             nn.AdaptiveAvgPool2d(1),
             nn.Conv2d(planes, planes // reduction, kernel_size=1),
-            nn.ReLU(inplace=True),
+            nn.PReLU(),
             nn.Conv2d(planes // reduction, planes, kernel_size=1),
             nn.Sigmoid()
         )
@@ -367,12 +216,12 @@ class VideoCNN(nn.Module):
         self.frontend3D = nn.Sequential(
                 nn.Conv3d(1, 64, kernel_size=(5, 7, 7), stride=(1, 2, 2), padding=(2, 3, 3), bias=False),
                 nn.BatchNorm3d(64),
-                nn.ReLU(True),
+                nn.PReLU(),
                 nn.MaxPool3d(kernel_size=(1, 3, 3), stride=(1, 2, 2), padding=(0, 1, 1))
                 )
 
         # resnet
-        self.resnet = ResNet(BasicBlock, [3, 4, 6, 3], se=se)
+        self.resnet = ResNet(BasicBlock, [2, 2, 2, 2], se=se)
         # self.resnet = ResNet(Bottleneck, [3, 8, 36, 3], se=se)
         self.dropout = nn.Dropout(p=0.5)
 
@@ -392,15 +241,11 @@ class VideoCNN(nn.Module):
     
     def forward(self, x):
         b, t = x.size()[:2]
-        # print(x.shape)
+
         x = self.visual_frontend_forward(x)
-        # x = self.dropout(x)
-        # feat = x.view(b, -1, 512)
-        # print(x.shape)
+        
         x = x.view(b, -1, 512) 
         
-        # print(x.shape)
-        # x = x.view(b, -1, 2048)       
         return x
 
     def _initialize_weights(self):
